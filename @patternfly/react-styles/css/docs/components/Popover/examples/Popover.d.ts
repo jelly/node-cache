791a8d582b33636b03958aab63dbe2560b2ca87a
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-22 12:58:01 +0000

    9afefd01202320c2ae389d8935aed67073fbb14c3f4a87d090a725623e1f3202  package.json

---
+++ @patternfly/react-styles/css/docs/components/Popover/examples/Popover.d.ts
@@ -0,0 +1,7 @@
+import './Popover.css';
+declare const _default: {
+  "popover": "pf-v5-c-popover",
+  "wsCoreCPopover": "ws-core-c-popover",
+  "wsPreviewHtml": "ws-preview-html"
+};
+export default _default;
