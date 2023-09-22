a810dd91f7d42393be3c36c5a8e8b5454da9bce0
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-22 12:47:10 +0000

    33427fe15cf54665c842eea3004f197169043cf69fe157f449658be4e2bc1135  package.json

---
+++ @patternfly/react-table/dist/esm/components/Table/TableText.js
@@ -0,0 +1,53 @@
+import { __rest } from "tslib";
+import * as React from 'react';
+import styles from '@patternfly/react-styles/css/components/Table/table';
+import { css } from '@patternfly/react-styles';
+import { Tooltip } from '@patternfly/react-core/dist/esm/components/Tooltip';
+export var TableTextVariant;
+(function (TableTextVariant) {
+    TableTextVariant["div"] = "div";
+    TableTextVariant["nav"] = "nav";
+})(TableTextVariant || (TableTextVariant = {}));
+export var WrapModifier;
+(function (WrapModifier) {
+    WrapModifier["wrap"] = "wrap";
+    WrapModifier["nowrap"] = "nowrap";
+    WrapModifier["truncate"] = "truncate";
+    WrapModifier["breakWord"] = "breakWord";
+    WrapModifier["fitContent"] = "fitContent";
+})(WrapModifier || (WrapModifier = {}));
+export const TableText = (_a) => {
+    var { children = null, className = '', variant = 'span', wrapModifier = null, tooltip: tooltipProp = '', tooltipProps = {}, onMouseEnter: onMouseEnterProp = () => { }, focused = false } = _a, props = __rest(_a, ["children", "className", "variant", "wrapModifier", "tooltip", "tooltipProps", "onMouseEnter", "focused"]);
+    const Component = variant;
+    const textRef = React.createRef();
+    const [tooltip, setTooltip] = React.useState('');
+    const onMouseEnter = (event) => {
+        if (event.target.offsetWidth < event.target.scrollWidth) {
+            setTooltip(tooltipProp || event.target.innerText);
+        }
+        else {
+            setTooltip('');
+        }
+        onMouseEnterProp(event);
+    };
+    const onFocus = (element) => {
+        if (element.offsetWidth < element.scrollWidth) {
+            setTooltip(tooltipProp || element.innerText);
+        }
+        else {
+            setTooltip('');
+        }
+    };
+    const text = (React.createElement(Component, Object.assign({ ref: textRef, onMouseEnter: onMouseEnter, className: css(className, wrapModifier && styles.modifiers[wrapModifier], styles.tableText) }, props), children));
+    React.useEffect(() => {
+        if (focused) {
+            onFocus(textRef.current);
+        }
+        else {
+            setTooltip('');
+        }
+    }, [focused]);
+    return tooltip !== '' ? (React.createElement(Tooltip, Object.assign({ triggerRef: textRef, content: tooltip, isVisible: true }, tooltipProps), text)) : (text);
+};
+TableText.displayName = 'TableText';
+//# sourceMappingURL=TableText.js.map
