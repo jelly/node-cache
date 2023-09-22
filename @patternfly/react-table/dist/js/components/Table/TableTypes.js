ea4df11941987bae1b852e5186c52031e84ac6bd
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-22 12:58:06 +0000

    2f35c06bcd2dfbd834022acd9f70cd47cd753ea5439c1bddce8ae403fe6adbd3  package.json

---
+++ @patternfly/react-table/dist/js/components/Table/TableTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TableVariant = exports.TableGridBreakpoint = void 0;
+const Dropdown_1 = require('@patternfly/react-core/dist/js/components/Dropdown');
+const Dropdown_2 = require('@patternfly/react-core/dist/js/deprecated/components/Dropdown');
+const Button_1 = require('@patternfly/react-core/dist/js/components/Button');
+const Tooltip_1 = require('@patternfly/react-core/dist/js/components/Tooltip');
+var TableGridBreakpoint;
+(function (TableGridBreakpoint) {
+    TableGridBreakpoint["none"] = "";
+    TableGridBreakpoint["grid"] = "grid";
+    TableGridBreakpoint["gridMd"] = "grid-md";
+    TableGridBreakpoint["gridLg"] = "grid-lg";
+    TableGridBreakpoint["gridXl"] = "grid-xl";
+    TableGridBreakpoint["grid2xl"] = "grid-2xl";
+})(TableGridBreakpoint = exports.TableGridBreakpoint || (exports.TableGridBreakpoint = {}));
+var TableVariant;
+(function (TableVariant) {
+    TableVariant["compact"] = "compact";
+})(TableVariant = exports.TableVariant || (exports.TableVariant = {}));
+//# sourceMappingURL=TableTypes.js.map
