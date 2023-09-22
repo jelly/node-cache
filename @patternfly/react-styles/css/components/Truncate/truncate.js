0371311a92a35f4f25ceaee31e16324a4e5d0f38
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-22 12:57:58 +0000

    dc8934d30f5553caf727ecd3c4165a80d64e9e56f12baae1cc0152af9f0ef3c1  package.json

---
+++ @patternfly/react-styles/css/components/Truncate/truncate.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+require('./truncate.css');
+exports.default = {
+  "truncate": "pf-v5-c-truncate",
+  "truncateEnd": "pf-v5-c-truncate__end",
+  "truncateStart": "pf-v5-c-truncate__start"
+};
