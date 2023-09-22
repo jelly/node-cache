0ec419bfe42989cd5706f8da3103e00587d021ff
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-22 12:40:36 +0000

    33427fe15cf54665c842eea3004f197169043cf69fe157f449658be4e2bc1135  package.json

---
+++ @patternfly/react-styles/css/components/Table/table-scrollable.mjs
@@ -0,0 +1,13 @@
+import './table-scrollable.css';
+export default {
+  "modifiers": {
+    "borderRight": "pf-m-border-right",
+    "borderLeft": "pf-m-border-left",
+    "right": "pf-m-right",
+    "left": "pf-m-left"
+  },
+  "scrollInnerWrapper": "pf-v5-c-scroll-inner-wrapper",
+  "scrollOuterWrapper": "pf-v5-c-scroll-outer-wrapper",
+  "table": "pf-v5-c-table",
+  "tableStickyCell": "pf-v5-c-table__sticky-cell"
+};
