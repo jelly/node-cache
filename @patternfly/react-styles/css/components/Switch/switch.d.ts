1eb59b5b68de94a3b59dd0334de029110888b7bf
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-25 12:47:35 +0000

    9afefd01202320c2ae389d8935aed67073fbb14c3f4a87d090a725623e1f3202  package.json

---
+++ @patternfly/react-styles/css/components/Switch/switch.d.ts
@@ -0,0 +1,15 @@
+import './switch.css';
+declare const _default: {
+  "modifiers": {
+    "reverse": "pf-m-reverse",
+    "off": "pf-m-off",
+    "on": "pf-m-on"
+  },
+  "switch": "pf-v5-c-switch",
+  "switchInput": "pf-v5-c-switch__input",
+  "switchLabel": "pf-v5-c-switch__label",
+  "switchToggle": "pf-v5-c-switch__toggle",
+  "switchToggleIcon": "pf-v5-c-switch__toggle-icon",
+  "themeDark": "pf-v5-theme-dark"
+};
+export default _default;
