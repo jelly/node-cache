7303f0c6edba2f4fb8a0b111f29c0e560867c353
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-26 11:15:20 +0000

    7932f95a227a2c11b2f6a82ea21a12249abc3859f977dd4f0628262be3bd95c9  package.json

---
+++ @patternfly/react-styles/css/components/Accordion/accordion.mjs
@@ -0,0 +1,16 @@
+import './accordion.css';
+export default {
+  "accordion": "pf-v5-c-accordion",
+  "accordionExpandableContent": "pf-v5-c-accordion__expandable-content",
+  "accordionExpandableContentBody": "pf-v5-c-accordion__expandable-content-body",
+  "accordionToggle": "pf-v5-c-accordion__toggle",
+  "accordionToggleIcon": "pf-v5-c-accordion__toggle-icon",
+  "accordionToggleText": "pf-v5-c-accordion__toggle-text",
+  "modifiers": {
+    "displayLg": "pf-m-display-lg",
+    "bordered": "pf-m-bordered",
+    "expanded": "pf-m-expanded",
+    "fixed": "pf-m-fixed"
+  },
+  "themeDark": "pf-v5-theme-dark"
+};
