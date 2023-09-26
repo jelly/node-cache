f9a9c395d8da1a6fbfaff81372c91d4e5b1853e8
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-26 09:59:39 +0000

    9afefd01202320c2ae389d8935aed67073fbb14c3f4a87d090a725623e1f3202  package.json

---
+++ @patternfly/react-styles/css/components/Chip/chip.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+require('./chip.css');
+exports.default = {
+  "button": "pf-v5-c-button",
+  "chip": "pf-v5-c-chip",
+  "chipActions": "pf-v5-c-chip__actions",
+  "chipContent": "pf-v5-c-chip__content",
+  "chipIcon": "pf-v5-c-chip__icon",
+  "chipText": "pf-v5-c-chip__text",
+  "modifiers": {
+    "overflow": "pf-m-overflow",
+    "draggable": "pf-m-draggable"
+  },
+  "themeDark": "pf-v5-theme-dark"
+};
