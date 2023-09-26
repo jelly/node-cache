95379b3ced30f5f30dfde6ff389a5bdbd3861113
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-26 10:12:25 +0000

    9afefd01202320c2ae389d8935aed67073fbb14c3f4a87d090a725623e1f3202  package.json

---
+++ @patternfly/react-styles/css/docs/components/ModalBox/examples/ModalBox.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+require('./ModalBox.css');
+exports.default = {
+  "modalBox": "pf-v5-c-modal-box",
+  "wsCoreCModal": "ws-core-c-modal",
+  "wsPreviewHtml": "ws-preview-html"
+};
