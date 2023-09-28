46124e582230624345aa1cd3a440c9248599d9ef
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-28 14:58:38 +0000

    719757cb0ca186825012850ffff4a8c8cbf55f3f8e971ffb6f61f2ed181cbcdd  package.json

---
+++ @patternfly/react-tokens/dist/js/c_card_m_selectable_m_clickable_m_selected_BorderColor.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_card_m_selectable_m_clickable_m_selected_BorderColor = {
+  "name": "--pf-v5-c-card--m-selectable--m-clickable--m-selected--BorderColor",
+  "value": "#d2d2d2",
+  "var": "var(--pf-v5-c-card--m-selectable--m-clickable--m-selected--BorderColor)"
+};
+exports["default"] = exports.c_card_m_selectable_m_clickable_m_selected_BorderColor;
