43155c9086ae466d34c9431250bb8b5691eae670
GitHub Workflow <cockpituous@cockpit-project.org>
2023-09-29 07:44:17 +0000

    5a9587628ae8f4014ecbdadbd9e1687d3288980ddf19fca314fdc688bec73476  package.json

---
+++ @patternfly/react-styles/css/components/AboutModalBox/about-modal-box.d.ts
@@ -0,0 +1,16 @@
+import './about-modal-box.css';
+declare const _default: {
+  "aboutModalBox": "pf-v5-c-about-modal-box",
+  "aboutModalBoxBrand": "pf-v5-c-about-modal-box__brand",
+  "aboutModalBoxBrandImage": "pf-v5-c-about-modal-box__brand-image",
+  "aboutModalBoxClose": "pf-v5-c-about-modal-box__close",
+  "aboutModalBoxContent": "pf-v5-c-about-modal-box__content",
+  "aboutModalBoxHeader": "pf-v5-c-about-modal-box__header",
+  "aboutModalBoxStrapline": "pf-v5-c-about-modal-box__strapline",
+  "button": "pf-v5-c-button",
+  "modifiers": {
+    "plain": "pf-m-plain"
+  },
+  "themeDark": "pf-v5-theme-dark"
+};
+export default _default;
---
+++ @patternfly/react-tokens/dist/js/c_background_image_BackgroundPosition.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.c_background_image_BackgroundPosition = {
+  "name": "--pf-v5-c-background-image--BackgroundPosition",
+  "value": "bottom right",
+  "var": "var(--pf-v5-c-background-image--BackgroundPosition)"
+};
+exports["default"] = exports.c_background_image_BackgroundPosition;
